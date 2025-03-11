fcc850b77c17c469d4e6881afe3497c6c8949cdf
red <redmoart@gmail.com>
2025-03-10 19:25:32 -0500

    🔧Config: add initial pages and navbar component with Tailwind CSS integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Basicos',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes Numeros',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes Uncommon',
+    loadComponent: () =>
+      import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Custom',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
---
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uncommon-page',
+  imports: [],
+  templateUrl: './uncommon-page.component.html',
+  styleUrl: './uncommon-page.component.css'
+})
+export default class UncommonPageComponent {
+
+}
