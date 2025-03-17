2d8f3990435e438f59ed56b42ee7d6935757cf07
red <redmoart@gmail.com>
2025-03-17 18:36:39 -0500

    ✨Add: create card component and integrate it into uncommon page layout

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
 })
 export default class UncommonPageComponent {
 
