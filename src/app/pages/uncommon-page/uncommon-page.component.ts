fe0ed6ca5a770aee4b41ab1eb6db6ddfd73a2e82
red <redmoart@gmail.com>
2025-03-18 22:32:46 -0500

    ✨Update: enhance uncommon page with dynamic client greeting and gender-based invitation

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
-import { CardComponent } from "../../components/card/card.component";
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'John Doe',
+  gender: 'male',
+  age: 31,
+  address: 'Ottawa, ON, Canada',
+};
+
+const client2 = {
+  name: 'Jane Doe',
+  gender: 'female',
+  age: 25,
+  address: 'Toronto, ON, Canada',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
+  //i18n select
+  client = signal(client1);
 
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client = signal(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
 }
